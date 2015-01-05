26d8cd84d7285434e47e23ed123ef49d77686283
Piyush <piyush.mishra@sodelsolutions.com>
2015-01-05 18:20:55 +0530

    got some success in building a complete binary tree

--- src/main/scala/datastructures/BinaryTrees.scala
+++ src/main/scala/datastructures/BinaryTrees.scala
@@ -2,6 +2,7 @@ package datastructures
 
 object module extends App {
 
+  val data = Array(2, 5, 6, -1, 8, 11, 12, 13, 14, 15, 16, 17, 18, 19)
   case class Node(var value: Option[Int] = None, var leftTree: Option[Node] = None, var rightTree: Option[Node] = None) {
     def isFull = this.leftTree.isDefined && this.rightTree.isDefined
     def isOnlyLeft = this.leftTree.isDefined && !this.rightTree.isDefined
@@ -18,22 +19,41 @@ object module extends App {
     override def toString = rootNode.toString
 
     def insert(value: Int, node: Node = rootNode): Unit = if (node.value.isDefined) {
-
       if (!node.leftTree.isDefined) {
         node.leftTree = Some(Node(Some(value)))
-        println("node" + node.value + "says : " + node.leftTree.get.value + "is my child")
+        println("node" + node.value + "says : " + node.leftTree.get.value + "is my left child")
       } else if (!node.rightTree.isDefined) {
         node.rightTree = Some(Node(Some(value)))
-        println("node" + node.value + "says : " + node.rightTree.get.value + "is my child")
-      } else if (node.leftTree.get.isFull && node.rightTree.get.isFull)
+        println("node" + node.value + "says : " + node.rightTree.get.value + "is right my child")
+      } else if (isbalanced(node.leftTree) && calculateNumberOfNodes(node.leftTree , 0) == calculateNumberOfNodes(node.rightTree , 0))
         insert(value, node.leftTree.get)
-      else if (node.leftTree.get.isFull)
-        insert(value, node.rightTree.get) else insert(value, node.leftTree.get)
+      else if (calculateNumberOfNodes(node.leftTree , 0) < Math.pow(2, calculateHeight(node.leftTree, 0)) - 1)
+        insert(value, node.leftTree.get) else insert(value, node.rightTree.get)
     }
   }
 
-  val data = Array(2, 3, 4, 5, 6, 7, 8, 9)
+  def isbalanced(tree: Option[Node]) = {
+    if (!tree.isDefined) false
+    else {
+      val a = calculateNumberOfNodes(tree, 0) == Math.pow(2, calculateHeight(tree, 0)) - 1
+      a
+    }
+  }
+
+  def calculateNumberOfNodes(tree: Option[Node], acc: Int): Int = {
+    if (!tree.isDefined) acc else
+      acc + 1 + calculateNumberOfNodes(tree.get.leftTree, 0) + calculateNumberOfNodes(tree.get.rightTree, 0)
+  }
+
+  def calculateHeight(tree: Option[Node], acc: Int): Int = {
+    if (!tree.isDefined) acc
+    else if (tree.get.isLeaf) acc + 1
+    else List(calculateHeight(tree.get.leftTree, acc + 1), calculateHeight(tree.get.rightTree, acc + 1)).max
+  }
 
   val tree = new BinaryTree(Some(1))
   data.foreach { i => tree.insert(i) }
+  println(calculateHeight(Some(tree.rootNode), 0))
+  println(calculateNumberOfNodes(Some(tree.rootNode), 0))
+  println(isbalanced(Some(tree.rootNode)))
 }
