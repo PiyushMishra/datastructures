2c0978010fcf594fa16af876f9dd20d832b9945f
Piyush <piyush.mishra@sodelsolutions.com>
2015-01-05 13:23:02 +0530

    skeleton tree node data structure

--- src/main/scala/datastructures/BinaryTrees.scala
+++ src/main/scala/datastructures/BinaryTrees.scala
@@ -1,37 +1,39 @@
 package datastructures
 
-object module {
+object module extends App {
 
-  abstract class TreeNode(val value: Option[Int], val leftTree: Option[TreeNode], val rightTree: Option[TreeNode]) {
-    def isLeaf: Boolean
+  case class Node(var value: Option[Int] = None, var leftTree: Option[Node] = None, var rightTree: Option[Node] = None) {
+    def isFull = this.leftTree.isDefined && this.rightTree.isDefined
+    def isOnlyLeft = this.leftTree.isDefined && !this.rightTree.isDefined
+    def isOnlyRight = !this.leftTree.isDefined && this.rightTree.isDefined
+    def isInner = this.leftTree.isDefined || this.rightTree.isDefined
+    def isLeaf = !this.leftTree.isDefined && !this.rightTree.isDefined
+    override def toString = "{value is : " + value + "\nleft : " + leftTree + "\nright : " + rightTree + "}"
   }
 
-  case class Leaf(override val value: Option[Int], override val leftTree: Option[TreeNode] = None,
-    override val rightTree: Option[TreeNode] = None)
-    extends TreeNode(value, leftTree, rightTree) {
-    def isLeaf = true
-  }
+  class BinaryTree(rootNodeValue: Option[Int]) {
 
-  case class SubTree(override val value: Option[Int] = None, override val leftTree: Option[TreeNode] = None,
-    override val rightTree: Option[TreeNode] = None)
-    extends TreeNode(value, leftTree, rightTree) {
-    def isLeaf = false
-  }
+    val rootNode = Node(rootNodeValue)
 
-  class BinaryTree {
-    val treeNode: TreeNode = new SubTree
+    override def toString = rootNode.toString
 
-    def insert(value: Int) = {
-      if (!treeNode.value.isDefined)
-        SubTree(Some(value), treeNode.leftTree, treeNode.rightTree)
-      else if (!treeNode.leftTree.isDefined) {
-        SubTree(treeNode.value, Some(SubTree(Some(value), None, None)), treeNode.rightTree)
-      } else SubTree(treeNode.value, treeNode.leftTree, Some(SubTree(Some(value), None, None)))
+    def insert(value: Int, node: Node = rootNode): Unit = if (node.value.isDefined) {
+
+      if (!node.leftTree.isDefined) {
+        node.leftTree = Some(Node(Some(value)))
+        println("node" + node.value + "says : " + node.leftTree.get.value + "is my child")
+      } else if (!node.rightTree.isDefined) {
+        node.rightTree = Some(Node(Some(value)))
+        println("node" + node.value + "says : " + node.rightTree.get.value + "is my child")
+      } else if (node.leftTree.get.isFull)
+        insert(value, node.rightTree.get)
+      else
+        insert(value, node.leftTree.get)
     }
   }
 
-  val data = Array(1, 2, 3, 4, 5, 6, 7)
+  val data = Array(2, 3, 5, 6, 7, 8)
 
-  println()
-
+  val tree = new BinaryTree(Some(1))
+  data.foreach { i => tree.insert(i) }
 }
