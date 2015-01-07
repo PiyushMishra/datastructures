f9c1a3ba652cb4c3df54fc406ae7c29c12542b97
Piyush <piyush.mishra@sodelsolutions.com>
2015-01-07 20:01:38 +0530

    finally done with heap sort

--- src/main/scala/datastructures/BinaryTrees.scala
+++ src/main/scala/datastructures/BinaryTrees.scala
@@ -121,7 +121,6 @@ object module extends App {
     }
 
     def swap2(node1: Node, node2: Node): Unit = {
-      val temp = node1.value
       node1.value = node2.value
       node2.value = Some(Int.MinValue)
     }
